72661102ecc7ecc00e83f16f39333f1329c6d9f3
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2015-10-24 18:16:08 -0400

    looping through capital bikeshare stations

--- extract_bikeshare_data.py
+++ extract_bikeshare_data.py
@@ -4,17 +4,53 @@ import code # to debug: `code.interact(local=locals())`
 import pybikes
 import json
 
-print("GETTING BIKE DATA...")
+
+#
+# REQUEST A LIST OF ALL KNOWN BIKESHARES
+#
+
+print "----------------------"
+print "GETTING BIKESHARES..."
+print "----------------------"
+
+# todo
+
+#
+# REQUEST AND PRINT INFO ABOUT EACH BIKESHARE
+#
 
 response = pybikes.get('capital-bikeshare')
 
-agency = response.meta
-feed_url = response.feed_url
-feed_type = response.method
+def list_of_encoded_strings(array_of_unicode_strings):
+    new_list = []
+    for str in array_of_unicode_strings:
+        new_list.append(str.encode())
+    return new_list
+
+bikeshare = {
+   'tag': response.tag.encode(),
+   'feed_url': response.feed_url.encode(),
+   'feed_method': response.method.encode(),
+   'city': response.meta["city"].encode(),
+   'name': response.meta["name"].encode(),
+   'country': response.meta["country"].encode(),
+   'companies': list_of_encoded_strings(response.meta["company"]),
+   'system': response.meta["system"].encode(),
+   'longitude': response.meta["longitude"],
+   'latitude':response.meta["latitude"]
+}
+print bikeshare
+
+#
+# REQUEST AND PRINT INFO ABOUT EACH STATION
+#
+
+print "----------------------"
+print "GETTING BIKE STATIONS..."
+print "----------------------"
 
 response.update()
 
-
-
-
-print("WRITING DATA TO FILE")
+for station in response.stations:
+    print station.to_json()
+    print "----------------------"
