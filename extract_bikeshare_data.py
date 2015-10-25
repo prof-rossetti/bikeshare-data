9c795bf1ac6eec2ac6d994a7abdb64cfc0d81875
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2015-10-24 21:58:05 -0400

    looping through api calls

--- extract_bikeshare_data.py
+++ extract_bikeshare_data.py
@@ -3,54 +3,70 @@
 import code # to debug: `code.interact(local=locals())`
 import os
 from pprint import pprint
-
 import pybikes
 import json
 import csv
 
 networks_dot_json = os.path.join(os.path.dirname(__file__), "fixtures/citybikes_api/get_networks.json")
-print "JSON FILE EXISTS" if os.path.isfile(networks_dot_json) else "OOPS"
-
 networks_dot_csv = os.path.join(os.path.dirname(__file__), "data/networks.csv")
-print "WRITING TO CSV FILE -- %(file_name)s" % {"file_name": networks_dot_csv}
 
+print "JSON FILE EXISTS" if os.path.isfile(networks_dot_json) else "OOPS"
+print "WRITING TO CSV FILE -- %(file_name)s" % {"file_name": networks_dot_csv}
 with open(networks_dot_json) as json_file:
     networks = json.load(json_file)
     for network in networks:
-        pprint(network)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        #pprint(network)
+        #try:
+        #  city_name = network["city"].encode()
+        #except UnicodeEncodeError:
+        #  city_name = "idk"
+        n = {
+          'id': network["id"],
+          'tag': network["tag"].encode(),
+          'name': network["name"].encode(),
+          'url': network["url"].encode(),
+          'city': network["city"], # city_name,
+          'lat': network["lat"],
+          'lng': network["lng"],
+          'radius': network["radius"]
+        }
+        pprint(n) #todo: write to .csv
 
 
 
 
 '''
-#
-# REQUEST AND PRINT INFO ABOUT EACH BIKESHARE "NETWORK"
-#
+        response = pybikes.get(n["tag"])
 
+
+        #code.interact(local=locals())
+
+        bikeshare = {
+           'tag': response.tag.encode(),
+           #'feed_url': response.feed_url,
+           #'feed_method': response.method.encode(),
+           'city': response.meta["city"],
+           'name': response.meta["name"],
+           'country': response.meta["country"],
+           'companies': response.meta["company"], # list_of_encoded_strings(response.meta["company"]),
+           #'system': response.meta["system"],
+           'longitude': response.meta["longitude"],
+           'latitude':response.meta["latitude"]
+        }
+
+        pprint(bikeshare)
+
+        response.update()
+
+        for station in response.stations:
+            print station.to_json()
+            print "----------------------"
+'''
+
+
+
+
+'''
 response = pybikes.get('capital-bikeshare')
 
 def list_of_encoded_strings(array_of_unicode_strings):
